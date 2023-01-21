60660f84970ed01e10488bd45142e6d31040c318
Prajin <prajin@showground.io>
2023-01-21 12:45:35 +0530

    updated feat and version

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ import { required, len } from './lib/base.js'
 class VuetifyValidator {
     constructor(context,i18n) {
         this.context = context
-        this.$t = i18n || this.context.$t || ((message) => message)
+        this.$t = i18n || this.context.$t || (message => message)
     }
     required(message='This Field is required !') {
-        return (v) => required(v) || this.context.$t(message)
+        return (v) => required(v) || this.$t(message)
     }
     /* Is true */
     isTrue(message='This field should be true !') {
-        return (v) => Boolean(v) || this.context.$t(message)
+        return (v) => Boolean(v) || this.$t(message)
     }
     /* Numeric */
     isNumeric(message='This Fields Should be numeric') {
-        return (v) => numeric(v) || this.context.$t(message)
+        return (v) => numeric(v) || this.$t(message)
     }
     /* Server error with */
     seWith(name) {
@@ -41,69 +41,72 @@ class VuetifyValidator {
     }
     // other custom rules
     isEmailOrPhone(message='Should be email or phone number') {
-        return (v) => email(v) || phone(v) || this.context.$t(message)
+        return (v) => email(v) || phone(v) || this.$t(message)
     }
 
     isEmail(message='Should be a valid email') {
-        return (v) => email(v) || this.context.$t(message)
+        return (v) => email(v) || this.$t(message)
     }
 
     isPhone(message='Should be a valid phone number') {
-        return (v) => phone(v) || this.context.$t(message)
+        return (v) => phone(v) || this.$t(message)
     }
 
     isStrongPassword(message='Please enter a strong password') {
-        return (v) => strongPassword(v) || this.context.$t(message)
+        return (v) => strongPassword(v) || this.$t(message)
     }
 
     isEquals(valueKey, message='Not matching !') {
-        return (v) => v === get(valueKey) || this.context.$t(message)
+        return (v) => v === get(this.context,valueKey) || this.$t(message)
     }
 
     min(minLength, message) {
-        return (v) => !v || len(v) >= minLength || this.context.$t(message)
+        return (v) => !v || len(v) >= minLength || this.$t(message)
     }
 
     max(maxLength, message) {
-        return (v) => !v || len(v) <= maxLength || this.context.$t(message)
+        return (v) => !v || len(v) <= maxLength || this.$t(message)
     }
 
     minValue(minLimit, message) {
         return (v) =>
-            parseFloat(v) >= parseFloat(get(minLimit)) ||
-            `${this.context.$t(message)} ${get(minLimit)}`
+            parseFloat(v) >= parseFloat(get(this.context,minLimit)) ||
+            `${this.$t(message)} ${get(this.context,minLimit)}`
     }
 
     maxValue(maxLimit, message) {
         return (v) =>
-            parseFloat(v) <= parseFloat(get(maxLimit)) ||
-            `${this.context.$t(message)} ${get(maxLimit)}`
+            parseFloat(v) <= parseFloat(get(this.context,maxLimit)) ||
+            `${this.$t(message)} ${get(this.context,maxLimit)}`
     }
 
     isValidName(message='Shoul be valid name !') {
-        return (v) => validName(v) || this.context.$t(message)
+        return (v) => validName(v) || this.$t(message)
     }
 
     isNumberWithMaxTwoDecimal(message) {
-        return (v) => numberWithMaxTwoDecimalPoint(v) || this.context.$t(message)
+        return (v) => numberWithMaxTwoDecimalPoint(v) || this.$t(message)
     }
 
 
     isAlpha(message) {
-        return (v) => alpha(v) || this.context.$t(message)
+        return (v) => alpha(v) || this.$t(message)
     }
     IsAlphaNumeric(message) {
-        return (v) => alphaNumeric(v) || this.context.$t(message)
+        return (v) => alphaNumeric(v) || this.$t(message)
     }
     IsPincode(message) {
-        return (v) => pincode(v) || this.context.$t(message)
+        return (v) => pincode(v) || this.$t(message)
     }
 
     IsValidFile(maxSize, allowedTypes, message='Should be a valid file!') {
         return (v) =>
-            (v.size <= maxSize && allowedTypes.includes(v.type)) || this.context.$t(message)
+            (v.size <= maxSize && allowedTypes.includes(v.type)) || this.$t(message)
     }
 
+    matchRegex(expr, message) {
+        return (v) => expr.test(v) || this.$t(message)
+    }
 }
 
 export default VuetifyValidator
